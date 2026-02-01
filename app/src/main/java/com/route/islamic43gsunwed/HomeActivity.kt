b70a8360fb209429357c9f7e008e98e5003017ec
Abdallah El-Zoka <abdallah.elzoka@gmail.com>
2026-02-01 20:23:24 +0200

    + Finished bottom navigation View + Added only empty fragments

---
+++ app/src/main/java/com/route/islamic43gsunwed/HomeActivity.kt
@@ -0,0 +1,51 @@
+package com.route.islamic43gsunwed
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.route.islamic43gsunwed.databinding.ActivityHomeBinding
+import com.route.islamic43gsunwed.fragments.HadethFragment
+import com.route.islamic43gsunwed.fragments.QuranFragment
+import com.route.islamic43gsunwed.fragments.RadioFragment
+import com.route.islamic43gsunwed.fragments.TasbeehFragment
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) { // R.id.navigation_quran
+                R.id.navigation_quran -> {
+
+                    pushFragment(QuranFragment())
+                }
+
+                R.id.navigation_radio -> {
+                    pushFragment(RadioFragment())
+                }
+
+                R.id.navigation_hadeth -> {
+                    pushFragment(HadethFragment())
+                }
+
+                R.id.navigation_tasbeeh -> {
+                    pushFragment(TasbeehFragment())
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+        binding.bottomNavigationView.selectedItemId = R.id.navigation_quran
+    }
+
+    private fun pushFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.islamiFragmentContainer.id, fragment)
+            .commit()
+    }
+
+}
---
+++ app/src/main/java/com/route/islamic43gsunwed/fragments/TasbeehFragment.kt
@@ -0,0 +1,26 @@
+package com.route.islamic43gsunwed.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.route.islamic43gsunwed.databinding.FragmentHadethBinding
+import com.route.islamic43gsunwed.databinding.FragmentQuranBinding
+import com.route.islamic43gsunwed.databinding.FragmentTasbeehBinding
+
+class TasbeehFragment : Fragment() {
+    private lateinit var binding: FragmentTasbeehBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentTasbeehBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
