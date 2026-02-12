29418bcbe706aa6ae95a3b02f00b98a4284d830c
fathy_ibrahim <omarfathy202920@gmail.com>
2026-02-12 08:52:39 +0200

    Added Tasbeeh Fragment with counter logic

--- app/src/main/java/com/route/islamic43gsunwed/fragments/TasbeehFragment.kt
+++ app/src/main/java/com/route/islamic43gsunwed/fragments/TasbeehFragment.kt
@@ -4,23 +4,56 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.route.islamic43gsunwed.databinding.FragmentHadethBinding
-import com.route.islamic43gsunwed.databinding.FragmentQuranBinding
-import com.route.islamic43gsunwed.databinding.FragmentTasbeehBinding
+import com.route.islamic43gsunwed.R
 
 class TasbeehFragment : Fragment() {
-    private lateinit var binding: FragmentTasbeehBinding
+
+    private lateinit var tvCounter: TextView
+    private lateinit var tvZikr: TextView
+    private lateinit var imgSebha: ImageView
+
+    private var count = 0
+    private var zikrIndex = 0
+
+    private val azkar = arrayOf(
+        "سبحان الله",
+        "الحمد لله",
+        "الله أكبر"
+    )
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentTasbeehBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        val view = inflater.inflate(R.layout.activity_sebha, container, false)
+
+        tvCounter = view.findViewById(R.id.tvCounter)
+        tvZikr = view.findViewById(R.id.tvZikr)
+        imgSebha = view.findViewById(R.id.imgSebha)
+
+        imgSebha.setOnClickListener {
+
+            count++
+            tvCounter.text = count.toString()
+
+            if (count == 33) {
+                count = 0
+                zikrIndex++
+
+                if (zikrIndex == azkar.size) {
+                    zikrIndex = 0
+                }
+
+                tvZikr.text = azkar[zikrIndex]
+                tvCounter.text = count.toString()
+            }
+        }
+
+        return view
     }
 }
